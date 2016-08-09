bc83537092050056ddcaa24462c19fd71e70772a
David Engel <entrymissing@gmail.com>
2016-08-09 21:54:23 +0200

    Adding Ping Support

--- collectors.py
+++ collectors.py
@@ -2,17 +2,60 @@ import random
 import time
 import oauth2
 import imaplib
+import re
+import subprocess
 
 import private_keys
 
+
 class BaseCollector(object):
-    def collect_data(self):
-        raise NotImplementedError("Subclasses should implement this!")
+  def __init__(self, configs = {}):
+    self.setUp(configs)
 
+  def setUp(self, configs):
+    pass
+    
+  def collect_data(self):
+    raise NotImplementedError("Subclasses should implement this!")
+
+        
 class RandomCollector(BaseCollector):
-    def collect_data(self):
-        return ['', random.randint(0, 100), time.time()]
+  def collect_data(self):
+    return ['', random.randint(0, 100), time.time()]
 
+        
+class PingCollector(BaseCollector):
+  MAX_LINES = 1000
+
+  def setUp(self, configs = {}):
+    self.ping_targets = configs.get('ping_targets', ['google.com'])
+    self.num_pings = configs.get('num_pings', 5)
+
+  def collect_data(self):
+    cmd = [ fping, '-c', str(self.num_pings) ] + self.ping_targets
+    ping_proc = subprocess.Popen(cmd, bufsize=256, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  
+    data_points = []
+  
+    for l in range(self.MAX_LINES):
+      raw_line = ping_proc.stdout.readline()
+      if raw_line == '':
+        break
+
+      m = re.match('([^ ]*)\s* : xmt/rcv/%loss = \d+/\d+/(\d+)%, min/avg/max = (.*)/(.*)/(.*)', raw_line)
+      if m:
+        target = m.group(1).replace('.', '_')
+        loss = int(m.group(2))
+        min = float(m.group(3))
+        avg = float(m.group(4))
+        max = float(m.group(5))
+        data_points.append(['.' + target + '_min', min])
+        data_points.append(['.' + target + '_loss', loss])
+        data_points.append(['.' + target + '_avg', avg])
+        data_points.append(['.' + target + '_max', max])
+    return data_points
+
+    
 class EMailCollector(BaseCollector):
   def collect_data(self):
     resp = oauth2.RefreshToken(private_keys.GOOGLE_CLIENT_ID,
--- settings.py
+++ settings.py
@@ -1,6 +1,14 @@
 CARBON_SERVER = 'dashboard.entrymissing.net'
 CARBON_PORT = 2003
 
-monitors = [{'monitor': 'EMailCollector',
+monitors_back = [{'monitor': 'EMailCollector',
              'ts_name': 'data.mail',
              'frequency': 60*10}]
+
+monitors = [{'monitor': 'PingCollector',
+             'ts_name': 'data.ping',
+             'frequency': 20,
+             'config': {'ping_targets': ['192.168.0.1', '8.8.8.8',
+                                         'google.com', 'spiegel.de',
+                                         'scripts.mit.edu', 'mail.caltech.edu'],
+                        'num_pings': 5}}]
