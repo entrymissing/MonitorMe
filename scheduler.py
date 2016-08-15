6d36823ab0cd844586747ae14160773192e7332b
David Engel <entrymissing@gmail.com>
2016-08-15 22:14:20 +0200

    Switching to settings eval

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,9 @@
-import settings
-import monitor
-import time
 import socket
+import sys
+import time
+
+import monitor
+import private_keys
 
 def printerFunc(metric, data, ts):
   print metric, data, ts
@@ -11,14 +13,18 @@ def submitterFunc(metric, data, ts):
   
   print 'sending message: %s' % message,
   sock = socket.socket()
-  sock.connect((settings.CARBON_SERVER, settings.CARBON_PORT))
+  sock.connect((private_keys.CARBON_SERVER,
+                private_keys.CARBON_PORT))
   sock.sendall(message)
   sock.close()
 
+def main(argv):
+  with open(argv[0], 'r') as fp:
+    monitors = eval(fp.read())
 
-if __name__ == '__main__':
+  print monitors
   all_monitors = []
-  for mon in settings.monitors:
+  for mon in monitors:
     all_monitors.append(monitor.Monitor(mon['monitor'],
                                         mon['frequency'],
                                         mon['ts_name'],
@@ -28,3 +34,6 @@ if __name__ == '__main__':
     for m in all_monitors:
       m.monitor()
     time.sleep(60)
+
+if __name__ == '__main__':
+  main(sys.argv[1:])
