ecf91628872bce8135ca30058ef5d006823f0671
David Engel <entrymissing@gmail.com>
2016-09-24 22:46:38 +0200

    Remove more unused code

--- monitor.py
+++ monitor.py
@@ -9,12 +9,10 @@ class Monitor(object):
   def __init__(self, monitor_name, ts_name, config, report_callback):
     self._monitor = getattr(collectors, monitor_name)(config)
     self.ts_name = ts_name
-    self._last_start = 0
     self._report_callback = report_callback
     self._done = True
 
   def report_data(self):
-    self._last_start = time.time()
     self._done = False
     data = self._monitor.collect_data()
     for name_ext, val, ts in data:
